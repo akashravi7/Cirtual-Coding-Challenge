fc1fddcfdf3638f822245b935ab86c0e3bdd3b50
Akash Ravi <akashravi7@gmail.com>
2017-04-13 19:48:53 -0400

    Included readme file

---
+++ README.txt.txt
@@ -0,0 +1,3 @@
+1. Download the zip folder from github
+2. Extract it
+3. Open index.html in the browser
--- controller.js
+++ controller.js
@@ -1,150 +1,8 @@
-var object;
-/*{"Data": [{
-  "Name": "Danny Michell",
-  "Technician": "Wayne George",
-  "Order_Date": "05 Sep 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(504) 313-5430",
-  "Email": "august.luettgen@brakus.biz",
-  "Order": "Done"
-},{
-  "Name": "Tyler Sherman",
-  "Technician": "Troy Stephens",
-  "Order_Date": "17 Sep 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(560) 432-5431",
-  "Email": "korey.white@gmail.com",
-  "Order": "Done"
-},{
-  "Name": "Eddie Moody",
-  "Technician": "Jeff Kim",
-  "Order_Date": "17 Apr 2016",
-  "Type": "QUOTE",
-  "Cell_Number": "(534) 253-9943",
-  "Email": "jayde_kemmer@yahoo.com",
-  "Order": "Done"
-},{
-  "Name": "Pearl Mann",
-  "Technician": "Charles Morrison",
-  "Order_Date": "08 Apr 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "",
-  "Email": "albertha.cassin@yahoo.com",
-  "Order": "Done"
-},{
-  "Name": "Bess Gray",
-  "Technician": "Charles Morrison",
-  "Order_Date": "10 Feb 2016",
-  "Type": "QUOTE",
-  "Cell_Number": "(336) 543-4521",
-  "Email": "akeem.hermiston@gmail.com",
-  "Order": "In Progress"
-},{
-  "Name": "Minerva Ray",
-  "Technician": "Thomas Davis",
-  "Order_Date": "15 Jun 2016",
-  "Type": "INSURANCE",
-  "Cell_Number": "(153) 900-3884",
-  "Email": "rutherford.fredrick@darrion.info",
-  "Order": "In Progress"
-},{
-  "Name": "Agnes Poole",
-  "Technician": "Jeff Kim",
-  "Order_Date": "20 Jan 2016",
-  "Type": "INSURANCE",
-  "Cell_Number": "(248) 487-7777",
-  "Email": "ashly_mertz@gmail.com",
-  "Order": "In Progress"
-},{
-  "Name": "Maria Castro",
-  "Technician": "Charles Morrison",
-  "Order_Date": "17 Apr 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(194) 538-4563",
-  "Email": "camila_mante@yahoo.com",
-  "Order": "In Progress"
-},{
-  "Name": "Allen McLaughlin",
-  "Technician": "Charles Morrison",
-  "Order_Date": "08 Dec 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "",
-  "Email": "",
-  "Order": "In Progress"
-},{
-  "Name": "Lydia Vargas",
-  "Technician": "Jeff Kim",
-  "Order_Date": "29 Aug 2016",
-  "Type": "WARRANTY",
-  "Cell_Number": "(240) 433-5513",
-  "Email": "rolando.connelly@yahoo.com",
-  "Order": "In Progress"
-},{
-  "Name": "Beatrice Hayes",
-  "Technician": "Wayne George",
-  "Order_Date": "19 Sep 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(113) 533-5543",
-  "Email": "cartwright.henry@alejandra.biz",
-  "Order": "Cancelled"
-},{
-  "Name": "Stella Ramos",
-  "Technician": "Jeff Kim",
-  "Order_Date": "20 Sep 2016",
-  "Type": "QUOTE",
-  "Cell_Number": "(504) 331-4352",
-  "Email": "demetrius.carter@keeley.name",
-  "Order": "In Progress"
-},{
-  "Name": "Jared Page",
-  "Technician": "Ivan Jenkins",
-  "Order_Date": "01 Jun 2016",
-  "Type": "QUOTE",
-  "Cell_Number": "(504) 312-6432",
-  "Email": "kade.sauer@hotmail.com",
-  "Order": "In Progress"
-},{
-  "Name": "Clyde Riley",
-  "Technician": "Wayne George",
-  "Order_Date": "11 Oct 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(222) 495-0305",
-  "Email": "brayan_muller@yahoo.com",
-  "Order": "In Progress"
-},{
-  "Name": "Dominic Francis",
-  "Technician": "Leonard Boone",
-  "Order_Date": "14 Jul 2016",
-  "Type": "WARRANTY",
-  "Cell_Number": "(242) 464-5673",
-  "Email": "feest.lessie@gmail.com",
-  "Order": "In Progress"
-},{
-  "Name": "Ola Lawson",
-  "Technician": "Charles Morrison",
-  "Order_Date": "25 Jul 2016",
-  "Type": "REPAIR",
-  "Cell_Number": "(888) 353-4642",
-  "Email": "klocko.virginia@daryl.tv",
-  "Order": "Cancelled"
-},{
-  "Name": "Pearl Rose",
-  "Technician": "Wayne George",
-  "Order_Date": "28 Jan 2016",
-  "Type": "QUOTE",
-  "Cell_Number": "(264) 435-2352",
-  "Email": "renner_harold@sheridan.ca",
-  "Order": "In Progress"
-},{
-  "Name": "Maxim Woods",
-  "Technician": "Jeff Kim",
-  "Order_Date": "30 Nov 2016",
-  "Type": "WARRANTY",
-  "Cell_Number": "(268) 563-6809",
-  "Email": "lehan_bedka@paris.tv",
-  "Order": "In Progress"
-}]};*/
+var object; //Stores the data to be loaded onto the table
 
+/**
+ *  This function sends a request to pull the JSON data from the gist file online.
+ **/
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -160,6 +18,9 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
+/**
+ *This function is invoked onload of body of the page
+ **/
 function initialize(){
 		getJSON('https://gist.githubusercontent.com/akashravi7/ce6dbf5a91716613ec09aac2544b99d3/raw/23028c2ab28a430518c0dac267df51747d4b7d72/cirtual_challenge_data.json', function(err, data) {
 		if (err != null) {
@@ -171,6 +32,9 @@ function initialize(){
     });
 }
 
+/**
+ *Function to load the JSON data to the table 
+ **/
 function loaddata(x){
 	var obj={};
 	obj.Data=[];
@@ -238,6 +102,9 @@ function loaddata(x){
 	}	
 }
 
+/**
+ *This function is invoked for every letter entered/deleted in the searchbox
+ **/
 function search(){
 	var searchString=document.getElementById("search-box").value;
 	var filteredData=[];
@@ -253,6 +120,9 @@ function search(){
 	loaddata(filteredData);
 }
 
+/**
+ *This function is invoked when the order status is changed in the last column of the table
+ **/
 function updateTable(cell){
 	var rowNumber=cell.parentNode.parentNode.rowIndex-1;
 	if(cell.parentNode.parentNode.cells[1].innerHTML==object.Data[rowNumber].Name){
